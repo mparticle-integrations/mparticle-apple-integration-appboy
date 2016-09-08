8bf4391d044b1a329aeaf28d7e51cb11dfd86e28
Peter Jenkins <peterjenkins@users.noreply.github.com>
2016-09-08 15:57:01 -0400

    Update imports using __has_include

--- mParticle-Appboy/MPKitAppboy.h
+++ mParticle-Appboy/MPKitAppboy.h
@@ -17,11 +17,10 @@
 //
 
 #import <Foundation/Foundation.h>
-#ifdef COCOAPODS
-    #import "MPKitExecStatus.h"
-    #import "MPKitProtocol.h"
+#if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle.h>)
+#import <mParticle_Apple_SDK/mParticle.h>
 #else
-    #import <mParticle_Apple_SDK/mParticle.h>
+#import "mParticle.h"
 #endif
 
 
