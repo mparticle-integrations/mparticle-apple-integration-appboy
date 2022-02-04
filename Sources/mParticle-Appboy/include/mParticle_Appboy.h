ecb100a78a5f6efb37cf00bd4d8590aa8d023be6
bstalnaker <bstalnaker@mparticle.com>
2022-02-04 13:40:31 -0500

    fix: fix spm imports

--- Sources/mParticle-Appboy/include/mParticle_Appboy.h
+++ Sources/mParticle-Appboy/include/mParticle_Appboy.h
@@ -8,4 +8,8 @@ FOUNDATION_EXPORT const unsigned char mParticle_AppboyVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <mParticle_Appboy/PublicHeader.h>
 
+#if defined(__has_include) && __has_include(<mParticle_Appboy/MPKitAppboy.h>)
 #import <mParticle_Appboy/MPKitAppboy.h>
+#else
+#import "MPKitAppboy.h"
+#endif
